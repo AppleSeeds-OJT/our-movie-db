529e9f18311ac473543dd50cc1049320f826e5e5
Ohad Leshem <ohadlesh@gmail.com>
2020-11-24 11:30:44 +0200

    another small cleanup

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -14,6 +14,10 @@ function Home() {
     });
   }, []);
 
+  const getImgUrl = (posterPath) => {
+      return `https://image.tmdb.org/t/p/original/${posterPath}`
+  }
+
   return (
     <div>
       <h2>Top 20 Movies from TMDB:</h2>
@@ -21,7 +25,7 @@ function Home() {
         {state.movies.map((movie, index) => (
           <li key={index}>
             <div className="flex col">
-              <div><img className="poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt=""/></div>
+              <div><img className="poster" src={getImgUrl(movie.poster_path)} alt=""/></div>
               <div>Movie Name: {movie.original_title}</div>
               <Plot movieId={movie.id} />
             </div>
