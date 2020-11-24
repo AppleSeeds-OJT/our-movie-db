b7cbbbd28bc2e335f622ec83eaacebc3d4b3ce3a
Ohad Leshem <ohadlesh@gmail.com>
2020-11-24 12:32:21 +0200

    added support for MATERIAL-UI Cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MovieCard from "../../Components/MovieCard/index";
 import Plot from "../../Components/Plot/index";
 import service from "../../Services/service";
 
@@ -9,7 +10,7 @@ function Home() {
 
   useEffect(() => {
     service.query().then((results) => {
-        console.log(results.results);
+        // console.log(results.results);
       setState((state) => ({ ...state, movies: results.results }));
     });
   }, []);
@@ -23,13 +24,14 @@ function Home() {
       <h2>Top 20 Movies from TMDB:</h2>
       <ul>
         {state.movies.map((movie, index) => (
-          <li key={index}>
-            <div className="flex col">
-              <div><img className="poster" src={getImgUrl(movie.poster_path)} alt=""/></div>
-              <div>Movie Name: {movie.original_title}</div>
-              <Plot movieId={movie.id} />
-            </div>
-          </li>
+              <MovieCard name={movie.original_title} plot={<Plot movieId={movie.id} />} imgUrl={getImgUrl(movie.poster_path)}/>
+        //   <li key={index}>
+        //     <div className="flex col">
+        //       <div><img className="poster" src={getImgUrl(movie.poster_path)} alt=""/></div>
+        //       <div>Movie Name: {movie.original_title}</div>
+        //       <Plot movieId={movie.id} />
+        //     </div>
+        //   </li>
         ))}
       </ul>
     </div>
